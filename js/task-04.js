4feb7b20060d487c489ccbedf611e7393ab0b2e6
LuigiQ02 <140318152+LuigiQ02@users.noreply.github.com>
2024-02-27 23:14:46 +0800

    updated

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,21 @@
-// 1. Start
-// 2. Initialize counterValue with 0
+// // 1. Start
+// // 2. Initialize counterValue with 0
 
-// 3. Retrieve the button with data-action "decrement" and store it in btnDec
-// 4. Retrieve the button with data-action "increment" and store it in btnInc
-// 5. Retrieve the span element with id "value" and store it in valueSpan
+// // 3. Retrieve the button with data-action "decrement" and store it in btnDec
+// // 4. Retrieve the button with data-action "increment" and store it in btnInc
+// // 5. Retrieve the span element with id "value" and store it in valueSpan
 
-// 6. Attach a click event listener to btnDec
-//     6.1 When btnDec is clicked, do Steps 6.2 and 6.3
-//     6.2 Decrease counterValue by 1
-//     6.3 Update the text content of valueSpan with the new counterValue
+// // 6. Attach a click event listener to btnDec
+// //     6.1 When btnDec is clicked, do Steps 6.2 and 6.3
+// //     6.2 Decrease counterValue by 1
+// //     6.3 Update the text content of valueSpan with the new counterValue
 
-// 7. Attach a click event listener to btnInc
-//     7.1 When btnInc is clicked, do Steps 7.2 and 7.3
-//     7.2 Increase counterValue by 1
-//     7.3 Update the text content of valueSpan with the new counterValue
+// // 7. Attach a click event listener to btnInc
+// //     7.1 When btnInc is clicked, do Steps 7.2 and 7.3
+// //     7.2 Increase counterValue by 1
+// //     7.3 Update the text content of valueSpan with the new counterValue
 
-// 8. End
+// // 8. End
 
 let counterValue = 0;
 
